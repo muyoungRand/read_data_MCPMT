8afb61cef8d52b793b96182e63af4895a9dd703a
Rand Kim <kimmuyoung.97@gmail.com>
2022-11-21 19:23:20 +0800

    read_data updated to work for MCPMT.
    Involves upgrades of both the UI file and the python file.
    
    Added 3ion freqscan into testdata file

--- read_data.py
+++ read_data.py
@@ -1,17 +1,20 @@
 import sys
+import os
+
 from PyQt5.QtWidgets import QMainWindow,QApplication,QDialog, QApplication, QFileDialog
 from PyQt5.QtCore import pyqtSignal,pyqtSlot
+from PyQt5.QtCore import *
+from netgraph import EditableGraph
+
 from read_data_ui import *
 from open_file_ui import *
 from fit_functions_ui import *
-from PyQt5.QtCore import *
 
 from read_data_func import *
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
-import os
 
 sys.path.insert(0, 'fit')
 from fit import *
@@ -36,7 +39,7 @@ class dataPlot():
     def __init__(self, file_path):
         self.file_path = file_path
 
-        (self.x, self.y1, self.yerr1, self.y2, self.yerr2, self.y5, self.yerr5, self.y7, self.yerr7, self.y8, self. yerr9) = (None, None, None, None, None, None, None, None, None, None, None, None, None)
+        (self.x, self.y1, self.yerr1, self.y2, self.yerr2) = (None, None, None, None, None)
 
         self.ifProcessedData = False
         self.fitdata_x = None
@@ -49,13 +52,13 @@ class dataPlot():
     def get_fitdata(self):
         return (self.fitdata_x, self.fitdata_y)
 
-    def getdata(self):
+    def getdata(self, channels = [5, 7, 9]):
         if self.ifProcessedData == False:
-            (self.x, self.y1, self.yerr1, self.y2, self.yerr2, self.y5, self.yerr5, self.y7, self.yerr7, self.y8, self. yerr9) = self.read_data()
+            (self.x, self.y1, self.yerr1, self.y2, self.yerr2) = self.read_data(channels)
 
-        return (self.x, self.y1, self.yerr1, self.y2, self.yerr2, self.y5, self.yerr5, self.y7, self.yerr7, self.y8, self. yerr9)
+        return (self.x, self.y1, self.yerr1, self.y2, self.yerr2)
 
-    def read_data(self):
+    def read_data(self, channels):
         '''
         Process raw data
         Replace get_x_y by another processing_data script.
@@ -65,20 +68,23 @@ class dataPlot():
         '''
         print('Processing File', str(self.file_path))
 
-        (x, y1, err1, y2, err2, y5, err5, y7, err7, y9, err9) = get_x_y(self.file_path)
+        (x, y, err1, y2, err2) = get_x_y(self.file_path, channels = channels)
 
-        return (x, y1, err1, y2, err2, y5, err5, y7, err7, y9, err9)
+        return (x, y, err1, y2, err2)
 
     def getFilePath(self):
         return self.file_path
 
 class openFile(QDialog):
+    resized = QtCore.pyqtSignal()
+
     def __init__(self):
         super().__init__()
         self.ui = Ui_FileWindow()
         self.ui.setupUi(self)
         #self.ui.pushButton_AddFiles.clicked.connect(self.openFileDialog)
         self.ui.pushButton_RemoveFiles.clicked.connect(self.removeFile)
+        self.resized.connect(self.resizeFunction)
 
         self.ui.listWidget_SelectedFiles.setSelectionMode(
             QtWidgets.QAbstractItemView.ExtendedSelection
@@ -87,9 +93,7 @@ class openFile(QDialog):
         #self.ui.listWidget_SelectedFiles.itemClicked.connect(self.addItemtoQueuePlot)
 
     def openFileDialog(self):
-        fname = QFileDialog.getOpenFileNames(self, 'Open file','/home/ngchihuan/research/python/projects/read_data/test/data/')
         fname = QFileDialog.getOpenFileNames(self, 'Open file', '/home/')
-        #fname = QFileDialog.getOpenFileNames(self, 'Open file','/mnt/dzmitrylab/experiment/')
         print(fname)
         for i,f in enumerate(fname[0]):
             item = QtWidgets.QListWidgetItem(f)
@@ -117,9 +121,18 @@ class openFile(QDialog):
         temp = item.data(QtCore.Qt.UserRole)
         return temp
 
+    def resizeEvent(self, event):
+        self.resized.emit()
+        return super().resizeEvent(event)
+
+    def resizeFunction(self):
+        ref = self.ui.listWidget_SelectedFiles.height() # Ref point for buttons to refer to
+        self.ui.listWidget_SelectedFiles.setGeometry(QtCore.QRect(10, 10, self.width()-20, self.height()-150))
+        self.ui.horizontalLayoutWidget.setGeometry(10, ref+20, self.width()-20, 40)
 
 
 class mainWindow(QMainWindow):
+    resized = QtCore.pyqtSignal()
 
     def __init__(self):
         super().__init__()
@@ -165,8 +178,65 @@ class mainWindow(QMainWindow):
         self.openFitSqzVac_Ui.fitSignal.connect(self.fitSignal_clicked)
         self.openFitSqzVac_Ui.fitSignalDone.connect(self.fitSignal_done)
 
+        self.active_channels = []
+        self.ui.checkSCPMT.toggled.connect(self.pickChn0)
+        self.ui.checkCH5.toggled.connect(self.pickChn5)
+        self.ui.checkCH7.toggled.connect(self.pickChn7)
+        self.ui.checkCH9.toggled.connect(self.pickChn9)
 
+        self.ui.pushPlotReset.clicked.connect(self.resetplot)
+
+        self.resized.connect(self.resizeFunction)
+        
         self.show()
+
+    def pickChn0(self): 
+        if self.ui.checkSCPMT.isChecked():
+            if 0 not in self.active_channels:
+                self.active_channels.append(0)
+            else:
+                pass
+        else:
+            if 0 in self.active_channels:
+                self.active_channels.remove(0)
+            else:
+                pass
+    def pickChn5(self): 
+        if self.ui.checkCH5.isChecked():
+            if 1 not in self.active_channels:
+                self.active_channels.append(1)
+            else:
+                pass
+        else:
+            if 1 in self.active_channels:
+                self.active_channels.remove(1)
+            else:
+                pass
+    def pickChn7(self): 
+        if self.ui.checkCH7.isChecked():
+            if 2 not in self.active_channels:
+                self.active_channels.append(2)
+            else:
+                pass
+        else:
+            if 2 in self.active_channels:
+                self.active_channels.remove(2)
+            else:
+                pass
+    def pickChn9(self): 
+        if self.ui.checkCH9.isChecked():
+            if 3 not in self.active_channels:
+                self.active_channels.append(3)
+            else:
+                pass
+        else:
+            if 3 in self.active_channels:
+                self.active_channels.remove(3)
+            else:
+                pass
+
+    def resetplot(self):
+        self.plotFiles()
 
     @pyqtSlot(int,str)
     def fitSignal_clicked(self,i,Fit_type):
@@ -241,8 +311,6 @@ class mainWindow(QMainWindow):
             item.setData(QtCore.Qt.UserRole, temp) #Attached an object to each item in the QListWidgetItem
             self.openFileUi.ui.listWidget_SelectedFiles.addItem(item)
 
-
-
     def plotFiles(self):
         #selectedItems = self.openFileUi.ui.listWidget_SelectedFiles.selectedItems()
         selectedItems = self.openFileUi.retrieveSelectedObject()
@@ -251,27 +319,48 @@ class mainWindow(QMainWindow):
         for f in selectedItems:
             x, y1, yerr1, y2, yerr2 = f.getdata()
             xfit, yfit = f.get_fitdata()
-            self.qmc.plot(x, y1, f.getFilePath())
+
+            for i in self.active_channels:
+                if i == 0:
+                    self.qmc.plot(x, y1, 0, f.getFilePath())
+                else:
+                    if i == 1:
+                        chn = 5
+                    elif i == 2:
+                        chn = 7
+                    elif i == 3:
+                        chn = 9
+                    self.qmc.plot(x, y2[i-1], chn, f.getFilePath())
             #if (xfit != None) and (yfit != None):
                 #self.qmc.plot(xfit, y1, f.getFilePath())
             self.qmc.draw()
 
+    def resizeEvent(self, event):
+        self.resized.emit()
+        return super().resizeEvent(event)
+
+    def resizeFunction(self):
+        self.ui.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, self.width()-10, self.height()-80))
+        self.qmc.setGeometry(QtCore.QRect(0, 0, self.width()-20, self.height()-90))
+
+        
+
 
 class Qt5MplCanvas(FigureCanvas):
     """Class to represent the FigureCanvas widget"""
-    def __init__(self):
+    def __init__(self, parent = None, hold = False):
         # plot definition
         self.fig = Figure()
         self.axes = self.fig.add_subplot(111)
         FigureCanvas.__init__(self, self.fig)
         self.thereisfit=False
+        
 
-
-    def plot(self,x,y,file_path):
+    def plot(self,x,y,chn,file_path):
         #self.axes.legend()
         head, tail = os.path.split(file_path)
         #print(head,tail)
-        self.axes.plot(x,y,'-o',label=tail)
+        self.axes.plot(x,y,'-o',label=tail + ', Chn' + str(chn))
         self.axes.grid(b=True)
         self.axes.legend(loc='upper left')
 
