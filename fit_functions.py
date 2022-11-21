4b2ab5c48facfeab00f2db38fba18116c22e5d17
Rand Kim <kimmuyoung.97@gmail.com>
2022-11-21 19:40:33 +0800

    Delete unnecessary fit files

--- fit_functions.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def sin_fit(x, p, arg1):
-    [a, b, c] = p
-    [d, e] = arg1
-    return a * np.sin(b * x + c)
--- fit_superposition_fock_population_ui.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'fit_superposition_fock_population.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.2
-#
-# WARNING! All changes made in this file will be lost!
-
